a4f0437a8111ac0bbf659d405e78a4f0d92c3967
Gopesh Sharma <gopesh.sharma@gmail.com>
2024-05-01 09:20:11 -0400

    Initial Commit

---
+++ gitEclipseIntegrationProj01/src/main/java/live/learnjava/gitEclipseIntegrationProj01/App.java
@@ -0,0 +1,17 @@
+package live.learnjava.gitEclipseIntegrationProj01;
+
+import live.learnjava.gitEclipseIntegrationProj01.beans.Student;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Student student = new Student("Ram Kumar");
+        System.out.println(student);
+    }
+}
